39e00e7940a73ebe18ca9fe811ebbec878955060
Naresh <120189841+nareshkumarme@users.noreply.github.com>
2023-07-11 14:11:24 +0530

    Add files via upload
    Different ways of JavaScript code to find a single number in array

---
+++ Find single number in array/Script.js
@@ -0,0 +1,46 @@
+function singleNumber(nums) {
+   
+    let numbers = nums.sort();
+    let results = 0;
+    
+
+     for (const [index, number] of numbers.entries()){
+        if (
+            number !== numbers[index + 1] && numbers[index + 1] !== numbers[index + 2]
+            
+
+        ) {
+            results = numbers[index +1];
+            return results
+            
+        }
+        
+         
+        
+        }
+        
+
+}
+
+
+//  function singleNumber(nums) {
+//     let numbers = nums.sort();
+//     let result = 0;
+//     console.log(numbers)
+
+//     for( var i = 0 ; i <= numbers.length ;i++ ){
+
+//         if(numbers[i] !== numbers[i+1] && numbers[i+1] !== numbers[i+2]) {
+//             result = numbers[i+1];
+//             return result
+//         }
+//     }
+    
+// }
+
+
+var lastNumber = singleNumber([2,2,2,2,6,6,3,3,3,4,4,4,4,7,5,5,1,1,1,1,1,1,9,9,9])
+console.log(lastNumber)
+
+
+
